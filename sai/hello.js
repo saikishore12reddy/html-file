51f944f68f93fb849c801b2580ef25aa01a521d5
saikishore <vallapusai12@gmail.com>
2023-08-17 16:02:05 +0530

    first commit

---
+++ sai/hello.js
@@ -0,0 +1,11 @@
+var user={
+    id:1,
+    personalinfo:{
+        name:"sai",
+        address:{
+            street:"prathnagar",
+            country:"india" 
+        }
+    }
+}
+ console.log(user.personalinfo.address.country);
